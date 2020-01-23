8ae21b13484481cb5d590599d1366677d554be69
Joseph <jlrbuellv@gmail.com>
2020-01-22 21:48:33 -0500

    Do some simple validation

--- example/server.py
+++ example/server.py
@@ -1,22 +1,31 @@
-from flask import Blueprint, render_template, abort, Flask, Response
+from flask import Blueprint, render_template, abort, Flask, jsonify, request
 from jinja2 import TemplateNotFound
 
-from flaskdocs import API
+from flaskdocs import API, Route, Schema
 
 app = Flask(__name__)
 
-api = API()
+api = API(
+    name="example",
+    app=app,
+)
 
 simple_page = Blueprint('simple_page', __name__,
                         template_folder='templates')
 
-@simple_page.route('/', defaults={'page': 'index'})
-@simple_page.route('/<page>')
-def show(page):
-    try:
-        return Response("moo")
-    except TemplateNotFound:
-        abort(404)
+# @simple_page.route('/', defaults={'page': 'index'})
+def moo(animal):
+    return jsonify({animal: "moo"})
+
+@simple_page.route("/moo", methods=["POST"])
+def handler(*args, **kwargs):
+    return Route(
+        name="moo",
+        path="/moo",
+        func=moo,
+        body_schema=Schema({"animal": str}),
+        response_schema=Schema({})
+    ).handler(*args, **kwargs)
 
 
 app.register_blueprint(simple_page)
--- flaskdocs/__init__.py
+++ flaskdocs/__init__.py
@@ -1,11 +1,16 @@
-from typing import List
+from typing import List, Callable
+from .schema import Schema, SchemaError
+from flask import Flask, request, jsonify
+import warnings
 
 class API:
     def __init__(
             self,
             name: str,
+            app: Flask,
             description: str = None,
     ):
+        self.app = app
         self.name = name
         self.routes = {}
 
@@ -14,6 +19,9 @@ class API:
             path,
             name=None,
             methods=None,
+            query_parameter_schema: Schema=None,
+            body_schema: Schema=None,
+            response_schema: Schema=None,
     ):
 
         self.routes[name] = Route(
@@ -32,9 +40,56 @@ class Route:
         self,
         name: str,
         path: str,
-        methods: List,
+        # method: str,
+        func: Callable,
+        query_parameter_schema: Schema=None,
+        body_schema: Schema=None,
+        response_schema: Schema=None,
     ):
         self.name = name
         self.path = path
         self.methods = []
+        self.func = func
+        self.query_parameter_schema = query_parameter_schema
+        self.body_schema = body_schema
+        self.response_schema = response_schema
+
+    def handler(self):
+        params = {}
+
+        try:
+            if self.query_parameter_schema:
+                params.update(
+                    self.query_parameter_schema.validate(
+                        request.args
+                ))
+        except SchemaError as err:
+            response = jsonify({"error": str(err)})
+            response.status_code = 422
+            return response
+
+        try:
+
+            if self.body_schema:
+                if request.is_json:
+                    params.update(
+                        self.body_schema.validate(
+                            request.json
+                    ))
+                else:
+                    raise NotImplementedError("Unknown body type, sorry")
+        except SchemaError as err:
+            response = jsonify({"error": str(err)})
+            response.status_code = 422
+            return response
+                    
+
+        response = self.func(**params)
+        if response.is_json and self.response_schema:
+            try:
+                self.response_schema.validate(response.json)
+            except Exception as err:
+                warnings.warn(f"Got error when validating the response: {err}")
+                
+        return response
 
